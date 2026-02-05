c42285d98de8ba3252a6f3c21d21b306fc01dfbf
Leandro Luz <leandroferreiraluz@gmail.com>
2026-02-05 08:35:26 -0300

    configura conexão banco app

---
+++ admin-panel/app/config/app.php
@@ -0,0 +1,13 @@
+<?php
+return [
+
+    'host' => "postgres",  # Nome do serviço no docker-compose
+    'schema' => "app",
+    'port' => "5432",  # Porta padrão do PostgreSQL
+    'name' => "atendente",  # Nome do banco de dados
+    'user' => "atendente",  # Usuário do banco
+    'pass' => "atendente123",  # Senha do banco
+    'type' => "pgsql",  # Tipo de banco de dados
+    'prep' => "1"  # Utilizar consultas preparadas
+    // 'slog' => "SystemSqlLog"  # Log SQL (opcional)
+];
